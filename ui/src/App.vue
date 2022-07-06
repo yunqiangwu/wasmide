76e74520b578a47382355b190626e462df77a1ab
13485 <842269153@qq.com>
2022-07-06 13:42:18 +0800

    feat: 添加 vue-router

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
+
+
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+      <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+    <p>
+      <!--使用 router-link 组件进行导航 -->
+      <!--通过传递 `to` 来指定链接 -->
+      <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+      <router-link to="/">Go to Home</router-link>
+      <router-link to="/about">Go to About</router-link>
+    </p>
+    <!-- 路由出口 -->
+    <!-- 路由匹配到的组件将渲染在这里 -->
+    <router-view></router-view>
+  </div>
 </template>
 
 <style>
--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,15 +1,44 @@
 import { createApp } from 'vue'
-import WasmTerminal from "@wasmer/wasm-terminal"
 import App from './App.vue'
-import 'xterm/css/xterm.css'
+import * as VueRouter from 'vue-router';
 
-createApp(App).mount('#app')
+import HelloWorld from './components/HelloWorld.vue'
+
+// 1. 定义路由组件.
+// 也可以从其他文件导入
+// const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+// 2. 定义一些路由
+// 每个路由都需要映射到一个组件。
+// 我们后面再讨论嵌套路由。
+const routes = [
+  { path: '/', component: HelloWorld },
+  { path: '/about', component: About },
+]
+
+// 3. 创建路由实例并传递 `routes` 配置
+// 你可以在这里输入更多的配置，但我们在这里
+// 暂时保持简单
+const router = VueRouter.createRouter({
+  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+  history: VueRouter.createWebHashHistory(),
+  routes, // `routes: routes` 的缩写
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
 
 
 // import { useState } from 'preact';
 // import{ fetchCommandFromWAPM } from "@wasmer/wasm-terminal";
 // import { lowerI64Imports } from "@wasmer/wasm-transformer";
 import { WasmFs } from '@wasmer/wasmfs'
+import WasmTerminal from "@wasmer/wasm-terminal"
+import 'xterm/css/xterm.css'
 
 const commandLoadMap: { [key: string]: string} = {
   hello: '/helloworld.wasm',
